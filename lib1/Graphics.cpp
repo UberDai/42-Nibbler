0996d275387b1fa823784e5b73865176d44904e0
Zleub <debray.arnaud@gmail.com>
2015-08-14 18:20:43 +0200

    ncurses clean

--- lib1/Graphics.cpp
+++ lib1/Graphics.cpp
@@ -6,7 +6,7 @@
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/07/22 00:24:33 by amaurer           #+#    #+#             */
-/*   Updated: 2015/07/30 05:06:20 by adebray          ###   ########.fr       */
+/*   Updated: 2015/08/14 18:16:23 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,72 +39,67 @@ void	Graphics::update(void)
 	unsigned	x;
 	unsigned	y;
 
-	// if (_snake.clockCountdown > 0) {
-	// 	return ;
-	// }
+	int ch;
 
-		int ch;
-
-		while ((ch = getch()) != -1)
-		{
-			if (ch == 112)
-				glib_action = PAUSE;
-			if (ch == 258)
-				glib_action = DOWN;
-			if (ch == 259)
-				glib_action = UP;
-			if (ch == 260)
-				glib_action = RIGHT;
-			if (ch == 261)
-				glib_action = LEFT;
-			if (ch == 50)
-				glib_action = LIB2;
-			if (ch == 51)
-				glib_action = LIB3;
-			printw("%d\n", ch);
-		}
+	while ((ch = getch()) != -1)
+	{
+		if (ch == 112)
+			glib_action = PAUSE;
+		if (ch == 258)
+			glib_action = DOWN;
+		if (ch == 259)
+			glib_action = UP;
+		if (ch == 260)
+			glib_action = RIGHT;
+		if (ch == 261)
+			glib_action = LEFT;
+		if (ch == 50)
+			glib_action = LIB2;
+		if (ch == 51)
+			glib_action = LIB3;
+	}
 
 
-	printw("\n"); // << std::endl;
-	printw("---------------\n"); // << "---------------" << std::endl;
-	printw("Size: %d\n", _snake.player.size); // << "Size: " << _snake.player.size << std::endl;
-	printw("Pending nom: \n", _snake.player.pendingNom); // << "Pending nom: " << _snake.player.pendingNom << std::endl;
-	printw("\n"); // << std::endl;
+	printw("\n");
+	printw("---------------\n");
+	printw("Size: %d\n", _snake.player.size);
+	printw("Pending nom: \n", _snake.player.pendingNom);
+	printw("\n");
 
 	y = 0;
 	while (y < _snake.level->height)
 	{
-		printw("   "); // << "   ";
+		printw("   ");
 		x = 0;
 		while (x < _snake.level->width)
 		{
 			switch (_snake.level->map[y][x])
 			{
 				case BLOCK_NONE:
-					printw(" "); // << ' ';
+					printw(" ");
 					break;
 				case BLOCK_WALL:
-					printw("/"); // << "\u25AF";
+					printw("/");
 					break;
 				case BLOCK_NOM:
-					printw("x"); // << "x";
+					printw("x");
 					break;
 				case BLOCK_HEAD:
-					printw("O"); // << 'O';
+					printw("O");
 					break;
 				default:
-					printw("o"); // << 'o';
+					printw("o");
 			}
-			printw(" "); // << ' ';
+			printw(" ");
 			x++;
 		}
 
-		printw("\n"); // << std::endl;
+		printw("\n");
 		y++;
 	}
 
-	printw("---------------\n"); // << "---------------" << std::endl;
-	printw("\n"); // << std::endl;
+	printw("---------------\n");
+	printw("\n");
 	refresh();
 	usleep(50000);
 }
